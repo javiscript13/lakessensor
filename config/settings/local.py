aad0fecfcf5a7759135e0ba401761b32c9e84098
Javier Antonio Hernandez De Paz <jahernandezdepaz@gmail.com>
2024-12-17 17:37:32 -0600

    moved local config variables to .django envs file

--- config/settings/local.py
+++ config/settings/local.py
@@ -15,6 +15,10 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django"]  # noqa: S104
+ALLOWED_HOSTS = env(
+    "DJANGO_ALLOWED_HOSTS",
+    default="['localhost', '0.0.0.0', '127.0.0.1', 'django']",
+)
 
 # CACHES
 # ------------------------------------------------------------------------------

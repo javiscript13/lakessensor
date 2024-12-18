c0b4cebcaf412e3010df524d9392b225014935bd
Javier Antonio Hernandez De Paz <jahernandezdepaz@gmail.com>
2024-12-18 07:40:07 -0600

    allowed hosts default value fix

--- config/settings/local.py
+++ config/settings/local.py
@@ -17,7 +17,7 @@ SECRET_KEY = env(
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django"]  # noqa: S104
 ALLOWED_HOSTS = env(
     "DJANGO_ALLOWED_HOSTS",
-    default="['localhost', '0.0.0.0', '127.0.0.1', 'django']",
+    default=['localhost', '0.0.0.0', '127.0.0.1', 'django'],
 )
 
 # CACHES

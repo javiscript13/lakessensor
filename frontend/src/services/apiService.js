cb7734385951a0845c4df33a8b08b0a4769da8ee
Javier Antonio Hernandez De Paz <jahernandezdepaz@gmail.com>
2025-04-16 22:49:53 -0600

    added bearer token to analog form, frontend and backend api

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,8 +1,14 @@
 import axiosInstance from './axiosInstance';
 
+const token = localStorage.getItem("access");
+
 export const postAnalogData = async (analogData) => {
     try {
-        const response = await axiosInstance.post('/sensor/analog', analogData);
+        const response = await axiosInstance.post('/sensor/analog', analogData, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
         return response.data;
     } catch (error) {
         throw new Error('Error posting data:', error);
--- sensorpipeline/views.py
+++ sensorpipeline/views.py
@@ -1,12 +1,14 @@
 from .models import Reading, AnalogReading
 from .serializers import ReadingSerializer, AnalogReadingSerializer
-from rest_framework import generics, viewsets, mixins
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 class ReadingCreate(generics.CreateAPIView):
     serializer_class = ReadingSerializer
 
 class AnalogReadingView(generics.ListCreateAPIView,
                         generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = AnalogReadingSerializer
     queryset = AnalogReading.objects.all()
     

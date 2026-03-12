3460609462e056fdfb3c9e0dead8118dc288d137
Javier Antonio Hernandez De Paz <jahernandezdepaz@gmail.com>
2026-03-12 12:18:51 -0600

    fixes for not flashihng between pages in fe

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from "./components/PrivateRoute";
 
-const Home = React.lazy(() => import(/* webpackPrefetch: true */ './pages/Home'));
-const About = React.lazy(() => import(/* webpackPrefetch: true */ './pages/About'));
-const Data = React.lazy(() => import(/* webpackPrefetch: true */ './pages/Data'));
-const DataForm = React.lazy(() => import(/* webpackPrefetch: true */ './pages/DataForm'));
-const Ressources = React.lazy(() => import(/* webpackPrefetch: true */ './pages/Ressources'));
-const Contact = React.lazy(() => import(/* webpackPrefetch: true */ './pages/Contact'));
-const NoMatch = React.lazy(() => import(/* webpackPrefetch: true */ './pages/NoMatch'));
+const homeModule = import(/* webpackPrefetch: true */ './pages/Home');
+const aboutModule = import(/* webpackPrefetch: true */ './pages/About');
+const dataModule = import(/* webpackPrefetch: true */ './pages/Data');
+const dataFormModule = import(/* webpackPrefetch: true */ './pages/DataForm');
+const ressourcesModule = import(/* webpackPrefetch: true */ './pages/Ressources');
+const contactModule = import(/* webpackPrefetch: true */ './pages/Contact');
+const noMatchModule = import(/* webpackPrefetch: true */ './pages/NoMatch');
+
+const Home = React.lazy(() => homeModule);
+const About = React.lazy(() => aboutModule);
+const Data = React.lazy(() => dataModule);
+const DataForm = React.lazy(() => dataFormModule);
+const Ressources = React.lazy(() => ressourcesModule);
+const Contact = React.lazy(() => contactModule);
+const NoMatch = React.lazy(() => noMatchModule);
 
 const AppRoutes = () => {
     return (

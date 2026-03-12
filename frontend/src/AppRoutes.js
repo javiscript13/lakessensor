dd40893df9ea897497cc5ebeab5c117093b8fb0f
Javier Antonio Hernandez De Paz <jahernandezdepaz@gmail.com>
2026-03-12 11:59:12 -0600

    fixes to stop flickering btween pages

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -2,29 +2,31 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from "./components/PrivateRoute";
 
-const Home = React.lazy(() => import('./pages/Home'));
-const About = React.lazy(() => import('./pages/About'));
-const Data = React.lazy(() => import('./pages/Data'));
-const DataForm = React.lazy(() => import('./pages/DataForm'));
-const Ressources = React.lazy(() => import('./pages/Ressources'));
-const Contact = React.lazy(() => import('./pages/Contact'));
-const NoMatch = React.lazy(() => import('./pages/NoMatch'));
+const Home = React.lazy(() => import(/* webpackPrefetch: true */ './pages/Home'));
+const About = React.lazy(() => import(/* webpackPrefetch: true */ './pages/About'));
+const Data = React.lazy(() => import(/* webpackPrefetch: true */ './pages/Data'));
+const DataForm = React.lazy(() => import(/* webpackPrefetch: true */ './pages/DataForm'));
+const Ressources = React.lazy(() => import(/* webpackPrefetch: true */ './pages/Ressources'));
+const Contact = React.lazy(() => import(/* webpackPrefetch: true */ './pages/Contact'));
+const NoMatch = React.lazy(() => import(/* webpackPrefetch: true */ './pages/NoMatch'));
 
 const AppRoutes = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/data" element={<Data />} />
-            <Route path="/form" element={
-                <PrivateRoute>
-                    <DataForm/>
-                </PrivateRoute>
-            }/>
-            <Route path="/ressources" element={<Ressources />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NoMatch />} />
-        </Routes>
+        <React.Suspense fallback={null}>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/data" element={<Data />} />
+                <Route path="/form" element={
+                    <PrivateRoute>
+                        <DataForm/>
+                    </PrivateRoute>
+                }/>
+                <Route path="/ressources" element={<Ressources />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NoMatch />} />
+            </Routes>
+        </React.Suspense>
     )
 }
 

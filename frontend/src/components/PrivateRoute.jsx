c8eb48778d76d50ca48a206074fcef9bfa0ee758
Javier Antonio Hernandez De Paz <jahernandezdepaz@gmail.com>
2026-04-23 16:48:23 -0600

    fixes on form spinner for production environmentn (without strict mode)

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import LoginPage from "../pages/LoginPage";
+import { CircularProgress, Grid } from "@mui/material";
 
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated, authLoading } = useAuth();
   const location = useLocation();
 
   if (authLoading) {
-    return null;
+    return (
+      <Grid container justifyContent="center" sx={{ paddingTop: 10 }}>
+        <CircularProgress />
+      </Grid>
+    );
   }
 
   if (isAuthenticated) {
